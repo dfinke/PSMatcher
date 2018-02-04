a685204660f56229231a227cfa2a9bba960545d8
Brian Scholer <brian@briantist.com>
2018-02-03 20:53:06 -0500

    Add support for bool-compatible result, clean up parameters, add pipeline support

--- PSMatcher.psm1
+++ PSMatcher.psm1
@@ -9,12 +9,60 @@ $null = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\$target\NMatcher.d
 $null = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\$target\Newtonsoft.Json.dll")
 $null = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\$target\Sprache.dll")
 
-function Test-Json {
+function New-BoolCompatibleResult {
+    [CmdletBinding()]
+    [OutputType([bool])]
     param(
-        $actual,
-        $test
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
+        [NMatcher.Matching.Result]
+        $Result
     )
 
-    $m = New-Object NMatcher.Matcher
-    $m.MatchJson($actual, $test) #.Successful
+    Begin {
+        $displayProperties = @('Successful', 'ErrorMessage') -as [String[]]
+        $propertySet = New-Object -TypeName System.Management.Automation.PSPropertySet -ArgumentList 'DefaultPropertySet', $displayProperties
+        $ddpStandardMembers = @($propertySet) -as [System.Management.Automation.PSMemberInfo[]]
+    }
+
+    Process {
+        $Result.Successful |
+            Add-Member -NotePropertyName Result -NotePropertyValue $result -TypeName NMatcher.Matching.Result -Force -PassThru |
+            Add-Member -MemberType ScriptProperty -Name Successful -Value { $this.Result.Successful } -Force -PassThru |
+            Add-Member -MemberType ScriptProperty -Name ErrorMessage -Value { $this.Result.ErrorMessage } -Force -PassThru |
+            Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $ddpStandardMembers -Force -PassThru
+    }
 }
+
+function Test-Json {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
+        [Alias('Actual')]
+        [ValidateNotNullOrEmpty()]
+        $Value ,
+
+        [Parameter(
+            Mandatory
+        )]
+        [Alias('Test')]
+        [ValidateNotNullOrEmpty()]
+        $Referecnce
+    )
+
+    Begin {
+        $matcher = New-Object -TypeName NMatcher.Matcher
+    }
+
+    Process {
+        $matcher.MatchJson($Value, $Referecnce) | New-BoolCompatibleResult
+    }
+}
+
+Export-ModuleMember -Function Test-Json
