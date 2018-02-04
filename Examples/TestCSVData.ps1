e461b173ca71d1849392655554186df73bcf5bc4
dfinke <finked@hotmail.com>
2018-02-04 11:47:38 -0500

    Use Test-Json to test PS Objects/CSV Data

---
+++ Examples/TestCSVData.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ..\PSMatcher.psd1 -Force
+
+$records = @"
+id,region,product,cost
+5d84babe-874e-4418-8cbb-f2c6ce74de92,North,Apple,1.29
+"@ | ConvertFrom-Csv
+
+$actual = $records | Select-Object id, region, product, @{n = 'cost'; e = {[double]$_.cost}} | ConvertTo-Json
+
+$tests = $(
+    '{"id":"@guid@","region":"North","product":"Apple","cost":"@int@"}'
+    '{"id":"@guid@","region":"North","product":"Apple","cost":"@double@"}'
+
+    '{"id":"@guid@","region":"North","product":"Apple","cost":"@double@.GreaterThan(1.00).LowerThan(1.29)"}'
+
+    '{"id":"@guid@","region":"North","product":"Apple","cost":"@double@.GreaterThan(1.00).LowerThan(1.30)"}'
+)
+
+$tests | ForEach-Object { $actual | Test-Json -Test $_ }
