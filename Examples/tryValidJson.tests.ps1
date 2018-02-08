e949ac4b1f85b95318f341620c5e5193eb0e9b36
dfinke <finked@hotmail.com>
2018-02-07 19:12:36 -0500

    Remove example test

--- Examples/tryValidJson.tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿describe 'Test JSON' {
-    It 'Should match first id' {
-        '{"Id":1,"name":"jane"}' | Should MatchJsonTemplate '{"Id":1,"name":"john"}'
-    }
-
-    It 'Should match second Id' {
-        '{"Id":2,"name":"john"}' | Should MatchJsonTemplate '{"Id":2,"name":"john"}'
-    }
-}
