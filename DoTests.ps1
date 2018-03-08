41a35ceea3a3a97f72981704088f0690b668ff73
dfinke <finked@hotmail.com>
2018-03-07 19:30:19 -0500

    changed scope for install

--- DoTests.ps1
+++ DoTests.ps1
@@ -1,7 +1,7 @@
 $PSVersionTable
 
 if ((Get-Module -ListAvailable pester) -eq $null) {
-    Install-Module -Name Pester -Repository PSGallery -Force
+    Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
 }
 
 Invoke-Pester
